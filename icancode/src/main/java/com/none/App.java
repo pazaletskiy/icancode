f05ff7402c290297e7a5e9c67b4a9d0153d0fa81
Dmitrii Minaev <dmitrii_minaev@epam.com>
2016-08-03 15:28:25 +0400

    initialize maven project

---
+++ icancode/src/main/java/com/none/App.java
@@ -0,0 +1,13 @@
+package com.none;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
